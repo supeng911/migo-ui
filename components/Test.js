e316d0378f33524e1515128695221d79621bc29b
speeder <1961335934@qq.com>
2018-11-06 12:07:56 +0800

    git init

---
+++ components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Text extends React.Component {
+    render() {
+        return (
+            <div>this is test</div>
+        );
+    }
+}
+
+export default Text;
