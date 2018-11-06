082d936ef665f3210c6a45a21fbf4ad3f7b1b48e
speeder <1961335934@qq.com>
2018-11-06 16:30:37 +0800

    1111

---
+++ src/scripts/emptyFolder.js
@@ -0,0 +1,6 @@
+// to delete existing built bundle with windows environment
+function emptyDist() {
+  return 'hello';
+}
+
+export default emptyDist();
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,23 @@
-const path = require('path');
-const webpack = require('webpack');
+/* eslint-disable no-var */
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'examples/index.js'),
   output: {
-    path: path.resolve(__dirname, 'examples/build'),
-    publicPath: 'http://127.0.0.1:8081/examples/build',
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
+    publicPath: '/',
+    libraryTarget: 'commonjs2', // necessary for the babel plugin
+    path: path.join(__dirname, 'lib-css'), // where to place webpack files
   },
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react', 'stage-2'],
-        },
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?modules&importLoaders=1&localIdentName=draftJsEmojiPlugin__[local]__[hash:base64:5]!postcss-loader' }),
       },
     ],
   },
+
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin({ filename: `${path.parse(process.argv[2]).name}.css` }),
   ],
 };
