f2ed65e689a290351e8eb28dc7c739e060eb617d
speeder <1961335934@qq.com>
2018-11-06 13:40:43 +0800

    test

---
+++ src/components/BaseBtn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class BaseBtn extends React.Component {
+  render() {
+    return (
+      <div>this is base button</div>
+    );
+  }
+}
+
+export default BaseBtn;
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class Text extends React.Component {
-    render() {
-        return (
-            <div>this is test</div>
-        );
-    }
+  render() {
+    return (
+      <div>this is test</div>
+    );
+  }
 }
 
 export default Text;
